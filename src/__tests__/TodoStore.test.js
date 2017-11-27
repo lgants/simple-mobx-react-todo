39863d58f6a8a6f498baf88c222ec23572d1c615
lgants <lgants@gmail.com>
2017-11-27 16:22:00 -0600

    cleanup

--- src/__tests__/TodoStore.test.js
+++ src/__tests__/TodoStore.test.js
@@ -1,12 +1,12 @@
-// import { TodoStore } from "../js/TodoStore"
-//
-// describe("TodoStore", () => {
-//   it("creates new todos", () => {
-//     const store = new TodoStore
-//     store.createTodo("todo1")
-//     store.createTodo("todo2")
-//     expect(store.todos.length).toBe(2)
-//     expect(store.todos[0].value).toBe("todo1")
-//     expect(store.todos[1].value).toBe("todo2")
-//   })
-// })
+import { TodoStore } from "../js/TodoStore"
+
+describe("TodoStore", () => {
+  it("creates new todos", () => {
+    const store = new TodoStore
+    store.createTodo("todo1")
+    store.createTodo("todo2")
+    expect(store.todos.length).toBe(2)
+    expect(store.todos[0].value).toBe("todo1")
+    expect(store.todos[1].value).toBe("todo2")
+  })
+})
